971e1b4a8bd74284d200f586aef0dd7382ead2e7
Vadim Balashov <balashov@corp.mail.ru>
2020-06-16 12:17:30 +0300

    Carthage support

---
+++ AppPear/apppear.h
@@ -0,0 +1,19 @@
+//
+//  apppear.h
+//  apppear
+//
+//  Created by Вадим Балашов on 16.06.2020.
+//  Copyright © 2020 sionyx. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for apppear.
+FOUNDATION_EXPORT double apppearVersionNumber;
+
+//! Project version string for apppear.
+FOUNDATION_EXPORT const unsigned char apppearVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <apppear/PublicHeader.h>
+
+
